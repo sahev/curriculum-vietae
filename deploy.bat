48cd3f8fb7b1e6df2d6e5c6550910f4df3c6c8f4
Samuel Henrique Evangelista <samuel.evangelista@dcnexxy.net.br>
2022-01-14 21:21:35 -0300

    update files

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+copy C:\projects\cv-vitae\git.sh C:\projects\cv-vitae\dist\git.sh
+cd C:\projects\cv-vitae\dist\git.sh
+bash git.sh
---
+++ git.sh
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "update files"
+git branch -M main
+git remote add origin https://github.com/sahev/curriculum-vietae-web.git
