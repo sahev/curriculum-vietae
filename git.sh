70bc4f82a4290d1afee191e1b9cf57cece67bc4e
Samuel Henrique Evangelista <samuel.evangelista@dcnexxy.net.br>
2022-01-14 21:47:20 -0300

    update files

--- git.sh
+++ git.sh
@@ -2,4 +2,5 @@ git init
 git add .
 git commit -m "update files"
 git branch -M main
-git remote add origin https://github.com/sahev/curriculum-vietae-web.git
+git remote add origin https://github.com/sahev/curriculum-vitae-web.git
+git push -f origin main
